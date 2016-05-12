d6d3ca4b0a37870aaafdd0e531fb9b5deef3b6ce
pagxir <pagxir@gmail.com>
2016-05-12 23:25:16 +0800

    client side dns server support

--- dns_forward.cpp
+++ dns_forward.cpp
@@ -13,6 +13,8 @@
 #include <utx/socket.h>
 #include <utx/dns_fwd.h>
 
+#include <tcpup/tcp_debug.h>
+
 struct dns_query_packet {
 	unsigned short q_ident;
 	unsigned short q_flags;
@@ -29,12 +31,15 @@ static struct cached_client {
 
 	int netif;
 	struct tcpup_addr from;
+
+	int hdrlen;
+	char header[64];
 } __cached_client[512];
 
 #define RCVPKT_MAXSIZ 1500
 static int __last_index = 0;
 
-int resolved_dns_packet(void *buf, const void *packet, size_t length, int *pnetif, struct tcpup_addr *from)
+int resolved_dns_packet(void *buf, const void *packet, size_t length, int *pnetif, struct tcpup_addr *from, rgn_iovec *vec)
 {
 	int flags;
 	int index, ident;
@@ -59,6 +64,8 @@ int resolved_dns_packet(void *buf, const void *packet, size_t length, int *pneti
 
 			*pnetif = client->netif;
 			memcpy(from, &client->from, sizeof(*from));
+			vec->iov_base = client->header;
+			vec->iov_len  = client->hdrlen;
 			return length;
 		}
 	}
@@ -66,7 +73,7 @@ int resolved_dns_packet(void *buf, const void *packet, size_t length, int *pneti
 	return 0;
 }
 
-int record_dns_packet(void *packet, size_t length, int netif, const tcpup_addr *from)
+int record_dns_packet(void *packet, size_t length, int netif, const tcpup_addr *from, void *hdr, size_t hdrlen)
 {
 	int flags;
 	struct dns_query_packet *dnsp;
@@ -86,6 +93,11 @@ int record_dns_packet(void *packet, size_t length, int netif, const tcpup_addr *
 	client->flags = 1;
 	client->l_ident = htons(dnsp->q_ident);
 	client->r_ident = (rand() & 0xFE00) | index;
+
+	client->hdrlen = hdrlen;
+	assert(hdrlen <= sizeof(client->header));
+	memcpy(client->header, hdr, hdrlen);
+
 	dnsp->q_ident = htons(client->r_ident);
 	return 1;
 }
@@ -110,21 +122,20 @@ struct udpuphdr {
 
     u_char  u_frag;
     u_char  u_doff;
+
+    u_char  u_tag;
+    u_char  u_len;
+
+    u_short u_port;
 };
 
 struct udpuphdr4 {
     struct udpuphdr uh;
-    u_char tag;
-    u_char len;
-    u_short port;
     u_int addr[1];
 };
 
 struct udpuphdr6 {
     struct udpuphdr uh;
-    u_char tag;
-    u_char len;
-    u_short port;
     u_int addr[4];
 };
 
@@ -174,13 +185,12 @@ static void on_udp_receive(void *upp)
 		salen = sizeof(saaddr);
 		len = recvfrom(ctx->uf_handle, packet, RCVPKT_MAXSIZ, MSG_DONTWAIT, (struct sockaddr *)&saaddr, &salen);
 		tx_aincb_update(&ctx->uf_aiocb, len);
-		fprintf(stderr, "udp packet: %d\n", len);
 		if (len > 0) {
 			rgn_iovec dns_pkt[2];
 			dns_pkt[0].iov_base = magic1;
 			dns_pkt[0].iov_len  = DNS_MAGIC_LEN;
 			if (len > 1420) {
-				fprintf(stderr, "packet is trim from %d to 1420\n", len);
+				TCP_DEBUG(len > 1420, "packet is trim from %d to 1420\n", len);
 				len = 1420;
 			}
 
@@ -191,9 +201,9 @@ static void on_udp_receive(void *upp)
 			up4->uh.u_doff = (sizeof(*up4) >> 2);
 			up4->uh.u_frag = 0;
 			up4->uh.u_flag = 0;
-			up4->tag = 0x84;
-			up4->len = 0x8;
-			up4->port = saaddr.sin_port;
+			up4->uh.u_tag = 0x84;
+			up4->uh.u_len = 0x8;
+			up4->uh.u_port = saaddr.sin_port;
 			up4->addr[0] = saaddr.sin_addr.s_addr;
 			assert(len + sizeof(*up4) < sizeof(udp_packet));
 			memcpy(up4 + 1, packet, len);
@@ -211,7 +221,6 @@ static void on_udp_receive(void *upp)
 struct udp_forward_context * udp_forward_create(int conv, int type)
 {
 	int err;
-	int bufsize = 8192;
 	struct sockaddr sa = {0};
 	struct udp_forward_context *ctx;
 	struct udp_forward_context_q *ctxq = &_forward_header;
@@ -231,6 +240,7 @@ struct udp_forward_context * udp_forward_create(int conv, int type)
 		assert(err == 0);
 
 #ifdef WIN32
+		int bufsize = 8192;
 		setsockopt(ctx->uf_handle, SOL_SOCKET, SO_RCVBUF, (char *)&bufsize, sizeof(bufsize));
 		tx_setblockopt(ctx->uf_handle, 0);
 #endif
@@ -254,48 +264,67 @@ struct udp_forward_context * udp_forward_create(int conv, int type)
 int filter_hook_dns_forward(int netif, void *buf, size_t len, const struct tcpup_addr *from)
 {
 	int err = -1;
-	int magic = htonl(0xfe800001);
-	unsigned char *pdns = 0;
+	int magic = -1;
+	struct udpuphdr *udphdr = NULL;
 
+	unsigned char *payload = (unsigned char *)buf;
+	unsigned char *payload_limit = (payload + len);
+
+	payload += DNS_MAGIC_LEN;
 	memcpy(&magic, buf, sizeof(magic));
-	if (magic == htonl(0xfe800001)) {
-		struct udpuphdr *up = (struct udpuphdr *)((char *)buf + DNS_MAGIC_LEN);
-		if (len >= DNS_MAGIC_LEN + sizeof(*up) && 
-				up->u_flag == 0 && up->u_magic == 0xcc && up->u_flag == 0) {
-			int doff = (up->u_doff << 2);
-			struct sockaddr_in target = {0};
-			struct udpuphdr4 *up4 = (struct udpuphdr4 *)up;
 
-			struct udp_forward_context *c = udp_forward_create(up->u_conv, up4->tag != 0x84);
-			if (up4->tag == 0x84 && c != NULL) {
+	if (magic == htonl(0xfe800001)) {
+
+		udphdr = (struct udpuphdr *)payload;
+		if (payload_limit - payload >= sizeof(*udphdr) && 
+				udphdr->u_flag == 0 && udphdr->u_magic == 0xcc) {
+			int doff = (udphdr->u_doff << 2);
+			struct sockaddr_in target = {0};
+			struct udpuphdr4 *uphdr4 = (struct udpuphdr4 *)udphdr;
+
+			struct udp_forward_context *c = udp_forward_create(udphdr->u_conv, udphdr->u_tag != 0x84);
+			if (udphdr->u_tag == 0x84 && c != NULL) {
 				target.sin_family = AF_INET;
-				target.sin_port   = (up4->port);
-				target.sin_addr.s_addr   = (up4->addr[0]);
-				err = sendto(c->uf_handle, (const char *)((char *)buf + DNS_MAGIC_LEN + doff),
-						len - DNS_MAGIC_LEN - doff, 0, (struct sockaddr *)&target, sizeof(target));
+				target.sin_port   = (udphdr->u_port);
+				target.sin_addr.s_addr   = (uphdr4->addr[0]);
+
+				err = sendto(c->uf_handle, (const char *)payload,
+						payload_limit - payload - doff, 0, (struct sockaddr *)&target, sizeof(target));
 				tx_timer_reset(&c->uf_timer, 120 * 1000);
 				c->uf_rcvtime = tx_getticks();
 				c->uf_from = *from;
-			} else {
-				fprintf(stderr, "found udp packet forward request\n");
 			}
 
+			TCP_DEBUG(udphdr->u_tag != 0x84, "found udp packet forward request\n");
 			return 1;
 		}
 	}
 
 	if (magic == htonl(0xfe800000)) {
-		pdns = (unsigned char *)buf;
-		if (record_dns_packet(pdns + DNS_MAGIC_LEN, len - DNS_MAGIC_LEN, netif, from)) {
-			err = sendto(_fwd_handle, (const char *)(pdns + DNS_MAGIC_LEN),
-					len - DNS_MAGIC_LEN, 0, (struct sockaddr *)&_fwd_target, sizeof(_fwd_target));
-			if (err == -1) {
-				fprintf(stderr, "sendto error %s\n", strerror(errno));
-				return 0;
-			}
-		}
 
-		return 1;
+		udphdr = (struct udpuphdr *)payload;
+		if (payload_limit - payload >= sizeof(*udphdr) && 
+				udphdr->u_flag == 0 && udphdr->u_magic == 0xcc) {
+			int doff = (udphdr->u_doff << 2);
+			struct sockaddr_in target = {0};
+			struct udpuphdr4 *udphdr4 = (struct udpuphdr4 *)udphdr;
+
+			memcpy(&target, &_fwd_target, sizeof(target));
+			if (udphdr->u_tag == 0x84 && udphdr4->addr[0] != 0x08080808) {
+				target.sin_family = AF_INET;
+				target.sin_port   = (udphdr->u_port);
+				target.sin_addr.s_addr   = (udphdr4->addr[0]);
+			}
+
+			if (record_dns_packet(payload + doff, payload_limit - payload - doff, netif, from, payload, doff)) {
+				err = sendto(_fwd_handle, (const char *)payload + doff,
+						payload_limit - payload - doff, 0, (struct sockaddr *)&target, sizeof(target));
+				TCP_DEBUG(err <= 0, "sendto error %s\n", strerror(errno));
+			}
+
+			TCP_DEBUG(udphdr->u_tag != 0x84, "found dns packet forward request\n");
+			return 1;
+		}
 	}
 
 	return 0;
@@ -321,15 +350,15 @@ static void on_dns_receive(void *upp)
 		tx_aincb_update(&_dns_aiocb, len);
 
 		if (len > 12) {
-			length = resolved_dns_packet(dns_packet, packet, len, &netif, &from);
+			rgn_iovec dns_pkt[3];
+			length = resolved_dns_packet(dns_packet, packet, len, &netif, &from, &dns_pkt[1]);
 			if (length > 12) {
-				rgn_iovec dns_pkt[2];
 				dns_pkt[0].iov_base = magic;
 				dns_pkt[0].iov_len  = DNS_MAGIC_LEN;
 
-				dns_pkt[1].iov_base = dns_packet;
-				dns_pkt[1].iov_len  = length;
-				utxpl_output(netif, dns_pkt, 2, &from);
+				dns_pkt[2].iov_base = dns_packet;
+				dns_pkt[2].iov_len  = length;
+				utxpl_output(netif, dns_pkt, 3, &from);
 			}
 		}
 	}
@@ -341,7 +370,6 @@ static void on_dns_receive(void *upp)
 static void module_init(void)
 {
 	int err;
-	int bufsize = 1024 * 1024;
 	struct sockaddr sa = {0};
 
 	_fwd_handle = socket(AF_INET, SOCK_DGRAM, 0);
@@ -351,6 +379,7 @@ static void module_init(void)
 	assert(err == 0);
 
 #ifdef WIN32
+	int bufsize = 1024 * 1024;
 	setsockopt(_fwd_handle, SOL_SOCKET, SO_RCVBUF, (char *)&bufsize, sizeof(bufsize));
 	tx_setblockopt(_fwd_handle, 0);
 #endif
@@ -358,7 +387,7 @@ static void module_init(void)
 	tx_loop_t *loop = tx_loop_default();
 	tx_aiocb_init(&_dns_aiocb, loop, _fwd_handle);
 
-	tx_task_init(&_dns_ready, loop, on_dns_receive, (void *)_fwd_handle);
+	tx_task_init(&_dns_ready, loop, on_dns_receive, (void *)(long)_fwd_handle);
 	tx_aincb_active(&_dns_aiocb, &_dns_ready);
 
 	_fwd_target.sin_family = AF_INET;
